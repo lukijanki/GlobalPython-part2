5d4733fec6351b7d53f25bef522888fde944c354
lukijanki <lukasz.jankowskikontakt@gmail.com>
2023-05-31 17:26:29 +0200

    poprawione zadanie1.3, poprawnie zlicza liczbe strzalow

--- 1. instrukcje_sterujące_wykonaniem_programu/main3v2.py
+++ 1. instrukcje_sterujące_wykonaniem_programu/main3v2.py
@@ -1,20 +1,21 @@
 from random import randint
 
+
 while True:
     losowana = randint(0, 100)
-    strzal = 0
+    liczba_strzalow = 0
     
     while True:
         strzal = int(input("Podaj liczbę od 0 do 100: "))
-        losowana += 1
+        liczba_strzalow += 1
 
         if strzal == losowana:
-            print(f"Gratulacje! Odgadłeś liczbę {losowana} w {strzal} strzałach.")
+            print(f"Gratulacje! Odgadłeś liczbę {losowana} w {liczba_strzalow} strzałach.")
             break
         elif strzal < losowana:
-            print("Liczba jest większa.")
+            print("Liczba jest większa od podanej przez Ciebie.")
         else:
-            print("Liczba jest mniejsza.")
+            print("Liczba jest mniejsza od podanej przez Ciebie.")
 
     dalej = input("Czy chcesz zagrać jeszcze raz? (tak/nie): ")
     if dalej.lower() != "tak":
